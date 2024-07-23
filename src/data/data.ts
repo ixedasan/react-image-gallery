ea64ef2bf0b21ce8e13e7b29ed3d3c71e5f9bf99
Yatsyshyn Oleksandr <pandikgenshin@gmail.com>
2024-07-23 21:15:48 +0300

    chore: add images

---
+++ src/data/data.ts
@@ -0,0 +1,49 @@
+import { IImageGallery } from '../types/global.types'
+
+export const initialImageData: IImageGallery[] = [
+  {
+    id: '1',
+    url: '/images/image-1.jpg',
+    isSelected: false,
+  },
+  {
+    id: '2',
+    url: '/images/image-2.jpg',
+    isSelected: false,
+  },
+  {
+    id: '3',
+    url: '/images/image-3.jpg',
+    isSelected: false,
+  },
+  {
+    id: '4',
+    url: '/images/image-4.jpg',
+    isSelected: false,
+  },
+  {
+    id: '5',
+    url: '/images/image-5.jpg',
+    isSelected: false,
+  },
+  {
+    id: '6',
+    url: '/images/image-6.jpg',
+    isSelected: false,
+  },
+  {
+    id: '7',
+    url: '/images/image-7.jpg',
+    isSelected: false,
+  },
+  {
+    id: '8',
+    url: '/images/image-8.jpg',
+    isSelected: false,
+  },
+  {
+    id: '9',
+    url: '/images/image-9.jpg',
+    isSelected: false,
+  },
+]
---
+++ src/types/global.types.ts
@@ -0,0 +1,5 @@
+export interface IImageGallery {
+  id: number | string
+  url: string
+  isSelected: boolean
+}
