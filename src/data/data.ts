c95595ae8087bb2cdd23b02e47262b47c198ec08
Yatsyshyn Oleksandr <pandikgenshin@gmail.com>
2024-07-25 22:14:02 +0300

    fix: images display

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,47 +3,47 @@ import { IImageGallery } from '../types/global.types'
 export const initialImageData: IImageGallery[] = [
   {
     id: '1',
-    url: '/images/image-1.jpg',
+    url: './images/image-1.jpg',
     isSelected: false,
   },
   {
     id: '2',
-    url: '/images/image-2.jpg',
+    url: './images/image-2.jpg',
     isSelected: false,
   },
   {
     id: '3',
-    url: '/images/image-3.jpg',
+    url: './images/image-3.jpg',
     isSelected: false,
   },
   {
     id: '4',
-    url: '/images/image-4.jpg',
+    url: './images/image-4.jpg',
     isSelected: false,
   },
   {
     id: '5',
-    url: '/images/image-5.jpg',
+    url: './images/image-5.jpg',
     isSelected: false,
   },
   {
     id: '6',
-    url: '/images/image-6.jpg',
+    url: './images/image-6.jpg',
     isSelected: false,
   },
   {
     id: '7',
-    url: '/images/image-7.jpg',
+    url: './images/image-7.jpg',
     isSelected: false,
   },
   {
     id: '8',
-    url: '/images/image-8.jpg',
+    url: './images/image-8.jpg',
     isSelected: false,
   },
   {
     id: '9',
-    url: '/images/image-9.jpg',
+    url: './images/image-9.jpg',
     isSelected: false,
   },
 ]
